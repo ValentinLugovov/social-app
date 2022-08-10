0b06591b68dd9521b8e622edf8335ad0893c076c
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-10 15:41:36 +0300

    New Header

---
+++ social/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import h from "./header.module.css";
+import logo from "../../img/github.png";
+
+const Header = () => {
+  return (
+    <div className={h.header}>
+      <img className={h.logo} src={logo}></img>
+    </div>
+  );
+};
+
+export default Header;
