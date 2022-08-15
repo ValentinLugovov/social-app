fae67157d0594e520c72f874367651690e9063aa
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-15 08:36:58 +0300

    Adds the friends component

---
+++ social/src/components/Friends/Friends.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./friends.module.css";
+
+const Friends = (props) => {
+  return <div className={s.friendsList}>dskfmsk</div>;
+};
+
+export default Friends;
