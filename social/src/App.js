79ed264aabd6555e812398c4ddb26c85860695bf
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-15 08:19:50 +0300

    Transfers data to a separate file Changes the path of the propses

--- social/src/App.js
+++ social/src/App.js
@@ -6,46 +6,35 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import Friends from "./components/Friends/Friends";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
-let dialogsData = [
-  { id: 1, name: "Will Smith" },
-  { id: 2, name: "Elon Musk" },
-  { id: 3, name: "Miley Cyrus" },
-  { id: 4, name: "Eren Yeager" },
-  { id: 5, name: "Vladimir Lenin" },
-];
-
-let messagesData = [
-  { id: 1, message: "Are watching my new film?" },
-  { id: 2, message: "Nice try, but my Tesla..." },
-  { id: 3, message: "May be u want go to the concert?" },
-  { id: 4, message: "AAAAAAaaaaAaAaaaA!!!!1!!1" },
-  { id: 5, message: "Я на бронивичке, жду тебя..." },
-];
-
-let postsData = [
-  { id: 1, name: "What are u doing tonight?", likeCount: "10" },
-  { id: 2, name: "Hey, how r u?", likeCount: "3" },
-];
 const App = (props) => {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Navbar />
+        <Navbar state={props.state.users} />
         <div className="app-content">
           <Routes>
             <Route
               path="/dialogs/*"
+              element={<Dialogs state={props.state.messagesPage} />}
+            />
+            <Route
+              path="/profile"
               element={
-                <Dialogs dialogs={dialogsData} messages={messagesData} />
+                <Profile
+                  profilePage={props.state.profilePage}
+                  addPost={props.addPost}
+                  updatePostText={props.updatePostText}
+                />
               }
             />
-            <Route path="/profile" element={<Profile posts={postsData} />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/friends" element={<Friends />} />
           </Routes>
         </div>
       </div>
