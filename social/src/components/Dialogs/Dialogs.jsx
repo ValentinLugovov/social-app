62aff1ebd0aeeb5c4a01ff60e55d19b3e27244c4
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-15 08:35:15 +0300

    Adds ref newMessageElement Adds message sending area

--- social/src/components/Dialogs/Dialogs.jsx
+++ social/src/components/Dialogs/Dialogs.jsx
@@ -4,15 +4,28 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-  let dialogs = props.dialogs.map((d) => (
+  let dialogs = props.state.dialog.map((d) => (
     <DialogItem name={d.name} id={d.id} />
   ));
-  let messages = props.messages.map((m) => <Message message={m.message} />);
+  let messages = props.state.message.map((m) => (
+    <Message message={m.message} />
+  ));
+
+  let newMessageElement = React.createRef();
+
+  let sendMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogItems}>{dialogs}</div>
       <div className={s.messages}>{messages}</div>
+      <div>
+        <textarea ref={newMessageElement}></textarea>
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
     </div>
   );
 };
