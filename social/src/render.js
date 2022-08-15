2aed56837d9fb0011c60604d408c1eec9113ec0f
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-15 08:21:57 +0300

    Renders the page and eliminates the import loop

---
+++ social/src/render.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { addPost, updatePostText } from "./redux/state";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+export let renderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updatePostText={updatePostText} />
+    </React.StrictMode>
+  );
+};
