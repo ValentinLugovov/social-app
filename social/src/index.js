fabf6135543211fdca3fd734831278c8253982fc
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-16 21:06:44 +0300

    Transfers rendering from the render.js

--- social/src/index.js
+++ social/src/index.js
@@ -1,7 +1,33 @@
 import reportWebVitals from "./reportWebVitals";
-import { renderEntireTree } from "./render";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  addMessage,
+  addPost,
+  subscribe,
+  updateMessage,
+  updatePostText,
+} from "./redux/state";
 import state from "./redux/state";
 
-renderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+let renderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updatePostText={updatePostText}
+        addMessage={addMessage}
+        updateMessage={updateMessage}
+      />
+    </React.StrictMode>
+  );
+};
+
+renderEntireTree(state);
+subscribe(renderEntireTree);
 reportWebVitals();
