1dab532968a4a558f9183ac9b5f8a506ea2713cb
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-23 13:49:41 +0300

    Transmits the store directly Removes the prop component Replaces components with containers

--- social/src/App.js
+++ social/src/App.js
@@ -2,43 +2,31 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import Friends from "./components/Friends/Friends";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import store from "./redux/redux-store";
+import FriendsContainer from "./components/Friends/FriendsContainer";
 
-const App = (props) => {
+const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Navbar state={props.state.users} />
+        <Navbar state={store.getState().users} />
         <div className="app-content">
           <Routes>
             <Route
               path="/dialogs/*"
-              element={
-                <Dialogs
-                  messagesPage={props.state.messagesPage}
-                  dispatch={props.dispatch}
-                />
-              }
+              element={<DialogsContainer store={store} />}
             />
-            <Route
-              path="/profile"
-              element={
-                <Profile
-                  profilePage={props.state.profilePage}
-                  dispatch={props.dispatch}
-                />
-              }
-            />
+            <Route path="/profile" element={<Profile store={store} />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path="/friends" element={<Friends />} />
+            <Route path="/friends" element={<FriendsContainer />} />
           </Routes>
         </div>
       </div>
