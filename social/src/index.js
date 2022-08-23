ada1f340b65be88a2ca5736fbcf1b1c874d31c78
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-23 13:50:50 +0300

    Adds the provider tag Removes page redrawing

--- social/src/index.js
+++ social/src/index.js
@@ -3,18 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from "./redux/state";
+import store from "./redux/redux-store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let renderEntireTree = (state) => {
-  root.render(
-    <React.StrictMode>
-      <App state={state} dispatch={store.dispatch.bind(store)} />
-    </React.StrictMode>
-  );
-};
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
-renderEntireTree(store.getState());
-store.subscribe(renderEntireTree);
 reportWebVitals();
