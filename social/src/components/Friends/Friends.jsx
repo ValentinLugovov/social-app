fd16d61bc0893ddf592a304196a13e711f5325db
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-23 13:55:55 +0300

    Renders the page Makes a server request

--- social/src/components/Friends/Friends.jsx
+++ social/src/components/Friends/Friends.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import s from "./friends.module.css";
+import * as axios from "axios";
 
 const Friends = (props) => {
-  return <div className={s.friendsList}>dskfmsk</div>;
+  axios
+    .get("https://social-network.samuraijs.com/api/1.0/users")
+    .then((response) => {
+      props.setUsers(response.date.items);
+    });
+
+  return (
+    <div className={s.friendsList}>
+      {props.friendsPage.map((u) => (
+        <div key={u.id}>
+          <div className={s.user}>
+            <div className={s.photoBlock}>
+              <img src={u.photo} className={s.img}></img>
+            </div>
+
+            <div className={s.nameBlock}>
+              <div className={s.fullname}>{u.fullname}</div>
+              <div className={s.status}>{u.status}</div>
+            </div>
+
+            <div className={s.locationBlock}>
+              <div className={s.location}>{u.location.city}</div>
+              <div className={s.location}>{u.location.country}</div>
+            </div>
+          </div>
+          <div className={s.btns}>
+            {u.followed ? (
+              <button className={s.button} onClick={() => props.unfollow(u.id)}>
+                UNFOLLOW
+              </button>
+            ) : (
+              <button className={s.button} onClick={() => props.follow(u.id)}>
+                FOLLOW
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Friends;
