d89cc881a880be26c5c1289c0ea9e57020a5911e
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-24 11:47:29 +0300

    Fixed

--- social/src/components/Friends/FriendsContainer.jsx
+++ social/src/components/Friends/FriendsContainer.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import Friends from "./Friends";
-import { connect } from "react-redux";
+import { connect} from "react-redux";
 import { followAC, unfollowAC, setUsersAC } from "../../redux/friendsReducer";
 
 const mapStateToProps = (state) => {
   return {
-    friendsPage: state.friendsPage.users,
+    friends: state.friendsPage.users,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
-      dispatch(followAC(userId))
+      dispatch(followAC(userId));
     },
     unfollow: (userId) => {
-      dispatch(unfollowAC(userId))
+      dispatch(unfollowAC(userId));
     },
     setUsers: (users) => {
-      dispatch(setUsersAC(users))
-    }
+      dispatch(setUsersAC(users));
+    },
   };
 };
+
 
 const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends);
 
