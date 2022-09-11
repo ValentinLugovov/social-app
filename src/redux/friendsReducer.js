5836f928ebc74c14fc2f129177226733304a0216
Valentin <valentinlugovov@gmail.com>
2022-09-11 20:57:57 +0300

    Deletes import img Adds action creater current page and total users count for pagination

--- src/redux/friendsReducer.js
+++ src/redux/friendsReducer.js
@@ -1,38 +1,14 @@
-import bale from "../img/bale.jpeg";
-import jason from "../img/jason.jpeg";
-import angelina from "../img/angelina.jpeg";
-
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 let initialState = {
-  users: [
-    // {
-    //   id: 1,
-    //   followed: true,
-    //   fullname: "Jason Stathem",
-    //   location: { city: "Saratov", country: "Great Britian" },
-    //   status: "Лучше иметь друга, чем друг друга...",
-    //   photo: jason,
-    // },
-    // {
-    //   id: 2,
-    //   followed: false,
-    //   fullname: "Angelina Jolie",
-    //   location: { city: "Minsk", country: "Belarus" },
-    //   status: "Beauty is my life!",
-    //   photo: angelina,
-    // },
-    // {
-    //   id: 3,
-    //   followed: true,
-    //   fullname: "Christian Bale",
-    //   location: { city: "Odessa", country: "Ukraine" },
-    //   status: "I'M THE BATMAN",
-    //   photo: bale,
-    // },
-  ],
+  users: [],
+  pageSize: 100,
+  totalUsersCount: 0,
+  currentPage: 1,
 };
 
 const friendsReducer = (state = initialState, action) => {
@@ -60,15 +36,38 @@ const friendsReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-        users: [...state.users, ...action.users],
+        users: action.users,
+      };
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.currentPage,
+      };
+    case SET_TOTAL_USERS_COUNT:
+      // делаем копию стейта и подменяем значение
+      return {
+        ...state,
+        totalUsersCount: action.count,
       };
     default:
       return state;
   }
 };
-
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
+// извне получаем данные и сетаем
+
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+/* action creater принимает текущую страницу и возвращает объект  */
+// по клику изменяем текущую страницу
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+//устанавливаем общее количество пользователей
+export const setUsersTotalCountAC = (totalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  count: totalUsersCount,
+});
 
 export default friendsReducer;
