c29eeb2d436c88a1ac9f8cc72af9354cefe18e96
Valentin <valentinlugovov@gmail.com>
2022-09-30 06:06:20 +0300

    Transfers FriendsContainer from Friends component Refactoring mapDispatchToProps Adds isFetching

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,45 +1,104 @@
 // контейнер скрывает стейт от компонент и передает данные через пропсы
-import Friends from "./Friends";
 import { connect } from "react-redux";
 import {
-  followAC,
-  unfollowAC,
-  setUsersAC,
-  setCurrentPageAC,
-  setUsersTotalCountAC,
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setUsersTotalCount,
+  toggleIsFetching,
 } from "../../redux/friendsReducer";
+import Friends from "./Friends";
+import * as axios from "axios";
+import React from "react";
+import Preloader from "../common/Preloader/Preloader";
+
+class FriendsContainer extends React.Component {
+  componentDidMount() {
+    // Запрашиваем информацию у сервера
+    this.props.toggleIsFetching(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+        this.props.setUsersTotalCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    this.props.toggleIsFetching(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Friends
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChanged={this.onPageChanged}
+          friends={this.props.friends}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+        />
+      </>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-  //достаем из стейта значения, перерисовываем и прокидываем обратно в компоненту
+  //достаем из стейта значения, перерисовываем и прокидываем обратно в FriendsContainer
   return {
     friends: state.friendsPage.users,
     pageSize: state.friendsPage.pageSize,
     totalUsersCount: state.friendsPage.totalUsersCount,
     currentPage: state.friendsPage.currentPage,
+    isFetching: state.friendsPage.isFetching,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  // возвращаем колбеки и диспатчит в стор
-  return {
-    follow: (userId) => {
-      dispatch(followAC(userId));
-    },
-    unfollow: (userId) => {
-      dispatch(unfollowAC(userId));
-    },
-    setUsers: (users) => {
-      dispatch(setUsersAC(users));
-    },
-    setCurrentPage: (pageNumber) => {
-      dispatch(setCurrentPageAC(pageNumber));
-    },
-    setUsersTotalCount: (totalCount) => {
-      dispatch(setUsersTotalCountAC(totalCount));
-    },
-  };
-};
+export default connect(mapStateToProps, {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setUsersTotalCount,
+  toggleIsFetching,
+})(FriendsContainer);
 
-const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends);
-
-export default FriendsContainer;
+// const mapDispatchToProps = (dispatch) => {
+//   // возвращаем колбеки и диспатчит в стор
+//   return {
+//     follow: (userId) => {
+//       dispatch(followAC(userId));
+//     },
+//     unfollow: (userId) => {
+//       dispatch(unfollowAC(userId));
+//     },
+//     setUsers: (users) => {
+//       dispatch(setUsersAC(users));
+//     },
+//     setCurrentPage: (pageNumber) => {
+//       dispatch(setCurrentPageAC(pageNumber));
+//     },
+//     setUsersTotalCount: (totalCount) => {
+//       dispatch(setUsersTotalCountAC(totalCount));
+//     },
+//     toggleIsFetching: (isFetching) => {
+//       dispatch(toggleIsFetchingAC(isFetching));
+//     },
+//   };
+// };
