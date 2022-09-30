74b499c5b7f699d365727ccf91c8f6ec4104b159
Valentin <valentinlugovov@gmail.com>
2022-09-30 06:10:02 +0300

    Adds user photo from API

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,14 +3,20 @@ import s from "./profileInfo.module.css";
 import avatar from "../../../img/avatar.jpeg";
 import bg1 from "../../../img/bg.jpeg";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
   return (
     <div>
       <div className={s.profileBlock}>
-        <img src={bg1} className={s.bg}></img>
+        <img src={bg1} className={s.bg} alt="bgimg"></img>
       </div>
-      <img src={avatar} className={s.avatar}></img>
+      {/* <img src={avatar} className={s.avatar} alt="avatar"></img> */}
       <div className={s.aboutMe}></div>
+      <img
+        src={props.profile.photos.large}
+        alt="avatar"
+        className={s.userPhoto}
+      ></img>
+      <p>{props.profile.fullName}</p>
     </div>
   );
 };
