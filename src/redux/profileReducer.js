797cd7c32be2956ff4c78b3b45d21a4c1f8d2fb7
Valentin <valentinlugovov@gmail.com>
2022-09-30 06:12:20 +0300

    Adds user profile for reducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_POST_TEXT = "UPDATE-POST-TEXT";
+const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 let initialState = {
   posts: [
@@ -7,6 +8,7 @@ let initialState = {
     { id: 2, name: "Nice day for working!!!", likeCount: "3" },
   ],
   postText: "",
+  profile: null,
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -23,13 +25,20 @@ const profileReducer = (state = initialState, action) => {
         ...state,
         postText: action.newText,
       };
+    case SET_USER_PROFILE:
+      return {
+        ...state,
+        profile: action.profile,
+      };
 
     default:
       return state;
   }
 };
 
+// функция, которая возвращает объект action(в котором находятся все данные для редьюсера )
 export const addPostActionCreater = () => ({ type: ADD_POST });
+export const setUsersProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 export const updatePostTextActionCreater = (text) => ({
   type: UPDATE_POST_TEXT,
   newText: text,
