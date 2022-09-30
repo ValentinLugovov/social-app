4e41fa47465b89d68699a395706f5b0984de7b22
Valentin <valentinlugovov@gmail.com>
2022-09-30 06:07:52 +0300

    Imports Preloader Adds props in ProfileInfo Adds condition for props.profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Preloader from "../common/Preloader/Preloader";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import s from "./profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = (props) => {
+  if (!props.profile) {
+    return <Preloader />;
+  }
+
   return (
     <div className={s.profileBlock}>
-      <ProfileInfo />
+      <ProfileInfo profile={props.profile} />
       <MyPostsContainer store={props.store} />
     </div>
   );
