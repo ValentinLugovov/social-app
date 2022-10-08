bf563afebce027a623b12febb0f4404863facffe
ValentinLugovov <valentinlugovov@gmail.com>
2022-10-09 00:36:00 +0300

    Adds "SIGN IN" button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import h from "./header.module.css";
+import s from "./header.module.css";
 import logo from "../../img/github.png";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div className={h.header}>
-      <img className={h.logo} src={logo}></img>
+    <div className={s.header}>
+      <img className={s.logo} src={logo} alt='logo'></img>
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login :
+        <NavLink to={'/login'} className={s.link}>SIGN IN</NavLink>}
+      </div>
     </div>
   );
 };
