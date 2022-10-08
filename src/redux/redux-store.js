06d8eacf75abf6a89df87e1c048bddde7619b04c
ValentinLugovov <valentinlugovov@gmail.com>
2022-10-09 01:09:23 +0300

    Adds authReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profileReducer";
 import messagesReducer from "./messagesReducer";
 import usersReducer from "./usersReducer";
 import friendsReducer from "./friendsReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   messagesPage: messagesReducer,
   users: usersReducer,
   friendsPage: friendsReducer,
+  auth: authReducer,
 });
 
 let store = createStore(reducers);
