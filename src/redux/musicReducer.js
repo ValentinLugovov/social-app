9b0352a7de6712d1cc0c8cc4b20c7e4a13a8b455
ValentinLugovov <valentinlugovov@gmail.com>
2022-10-13 23:15:18 +0300

    Adds reducer for music page

---
+++ src/redux/musicReducer.js
@@ -0,0 +1,58 @@
+const SET_TITLE = "SET_TITLE";
+const SET_SUBTITLE = "SET_SUBTITLE";
+const SET_IMAGE = "SET_IMAGE";
+const GET_TRACKS = "GET_TRACKS";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
+
+let initialState = {
+  title: "",
+  subtitle: "",
+  setImage: null,
+  tracks: [],
+  isFetching: false,
+};
+
+const musicReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case SET_TITLE:
+    //   return {
+    //     ...state,
+    //     title: action.title,
+    //   };
+    // case SET_SUBTITLE:
+    //   return {
+    //     ...state,
+    //     subtitle: action.subtitle,
+    //   };
+    // case SET_IMAGE:
+    //   return {
+    //     ...state,
+    //     image: action.image,
+    //   };
+    case GET_TRACKS:
+      return {
+        ...state,
+        tracks: action.tracks,
+      };
+    case TOGGLE_IS_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// функция, которая возвращает объект action(в котором находятся все данные для редьюсера )
+export const setTitle = (title) => ({ type: SET_TITLE, title });
+export const setSubTitle = (subtitle) => ({ type: SET_SUBTITLE, subtitle });
+export const setImage = (image) => ({ type: SET_IMAGE, image });
+export const getTracks = (tracks) => ({ type: GET_TRACKS, tracks });
+export const toggleIsFetching = (isFetching) => ({
+  type: TOGGLE_IS_FETCHING,
+  isFetching,
+});
+
+export default musicReducer;
