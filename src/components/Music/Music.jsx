5b70bd2b9acdcfc9e7bb80ef4d8e85164fc35a78
ValentinLugovov <valentinlugovov@gmail.com>
2022-10-13 23:12:48 +0300

    Adds top tracks on the music page

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import s from "./music.module.css";
 
 const Music = (props) => {
-  return <div>Music</div>;
+  return (
+    <div>
+      {props.tracks.map((t) => (
+        <div className={s.box}>
+          <div className={s.imgBox}>
+            <a href={t.share.href} target="_ blank">
+              <img src={t.share.image} alt="image" />
+            </a>
+          </div>
+          <div className={s.title}>
+            <div>{t.title}</div>
+            <div>{t.subtitle}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Music;
